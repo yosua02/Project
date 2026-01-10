18095135a9fb1fe7e97d5b3265d387c92a671d8f
yosua02 <yosuatahreak@gmail.com>
2026-01-10 17:34:26 +1100

    Add function to dynamically input value

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
+import React, {useState } from 'react';
 import './App.css';
-import TodoRowItem from './components/TodoRowItem';
 import TodoTable from './components/TodoTable';
+import NewTodoForm from './components/NewTodoForm';
 
 function App() {
 
-  const todos = [
+  const [todos, setTodos] = useState( [
     {rowNumber: 1, rowDescription: 'Feed puppy', rowAssigned: 'User One'},
     {rowNumber: 2, rowDescription: 'Water Plants', rowAssigned: 'User Two'},
-    {rowNumber: 3, rowDescription: 'Make Dinner', rowAssigned: 'User Three'}
-  ]
+    {rowNumber: 3, rowDescription: 'Make Dinner', rowAssigned: 'User Three'},
+    {rowNumber: 4, rowDescription: 'Buy Groceries', rowAssigned: 'User One'}
+  ])
+
+  const addTodo =(description, assigned) => {
+    if (todos.length>0){
+      const newTodo = {
+        rowNumber: todos.length +1,
+        rowDescription: description,
+        rowAssigned: assigned
+      };
+      setTodos(todos => [...todos, newTodo])
+    }
+  }
 
   return (
     <div className='mt-5 container'>
@@ -18,6 +31,10 @@ function App() {
         </div>
         <div className='card-body'>
           <TodoTable todos={todos}/>
+          <button className='btn btn-primary' onClick={(addTodo)}>
+            Add new todo
+            </button>
+            <NewTodoForm addTodo={addTodo}/>
         </div>
       </div>
     </div>
